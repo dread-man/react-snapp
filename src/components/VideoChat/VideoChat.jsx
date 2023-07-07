a83409e2209a2e7034bb17141a5fdd55f4f1c889
David <davidsokol112@gmail.com>
2023-07-07 23:50:59 +0300

    styles for icons in videoChat

--- src/components/VideoChat/VideoChat.jsx
+++ src/components/VideoChat/VideoChat.jsx
@@ -1,21 +1,48 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './VideoChat.module.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { getVideoChat } from '../../store/video/videoSlice'
+import { Link } from 'react-router-dom'
 
 const VideChat = () => {
+    const dispatch = useDispatch()
+    const [micro, setMicro] = useState(true)
+    const videoStore = useSelector((state) => state.video)
+
     useEffect(() => {
+        dispatch(getVideoChat())
         document.body.style.overflow = 'hidden'
     }, [])
+
+    const videoData = videoStore.videoData
+    console.log(videoData)
 
     return (
         <div className={styles.container}>
             <div className={styles.mainWindow}></div>
             <div className={styles.footer}>
-                <span>Test</span>
+                <span>{videoData && videoData.description}</span>
                 <div className={styles.buttons}>
                     <i className="ri-team-line"></i>
-                    <i className="ri-mic-line"></i>
-                    {/* <i className="ri-mic-off-line"></i> */}
-                    <i  className={`ri-phone-fill ${styles.exit}`}></i>
+                    {micro && (
+                        <i
+                            className="ri-mic-line"
+                            onClick={() => {
+                                setMicro(!micro)
+                            }}
+                        ></i>
+                    )}
+                    {!micro && (
+                        <i
+                            className={`ri-mic-off-line ${styles.offMicro}`}
+                            onClick={() => {
+                                setMicro(!micro)
+                            }}
+                        ></i>
+                    )}
+                    <Link className={styles.link} to="/">
+                        <i className={`ri-phone-fill ${styles.exit}`}></i>
+                    </Link>
                 </div>
             </div>
         </div>
