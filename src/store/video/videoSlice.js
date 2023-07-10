e010d03cf3f099036b8da70107c3a780b3a5ba32
David <davidsokol112@gmail.com>
2023-07-10 21:52:39 +0300

    test getVideoChatToken

--- src/store/video/videoSlice.js
+++ src/store/video/videoSlice.js
@@ -58,6 +58,38 @@ export const getVideoChatToken = createAsyncThunk(
 	}
 )
 
+export const getVideoChatToken1 = createAsyncThunk(
+	'video/getVideoChatToken',
+	async function (channelName, { rejectWithValue }) {
+		const url__video_chat__access_token = `http://16.162.236.210:3001/video-chat/access-token/${channelName}`
+
+		const requestOptions = {
+			method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('bearer')}`,
+            },
+		}
+
+		try {
+			const response = await fetch(url__video_chat__access_token, requestOptions)
+
+			if(!response.ok) {
+				throw new Error('Error with getting video token')
+			}
+			const data = await response.json()
+			console.log(data)
+
+			localStorage.setItem('VideoChatToken', data.access_token)
+
+
+		} catch (error) {
+			rejectWithValue(error.message)
+		}
+	}
+)
+
+
 
 
 const videoSlice = createSlice({
